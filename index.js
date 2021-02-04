63c96660de7e5a0abb49c4334f7d53e8ca07726d
EcaterinaNoroaca <56801157+EcaterinaNoroaca@users.noreply.github.com>
2021-02-04 13:30:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/*ex 1
+setTimeout(function(){for(var i=0; i<5; i++){console.log(i)}} ,1000)
+
+  ex 2
+ var s = new Set(arr);
+ console.log(s.size);
+ */
+
 //ex 3
 const arr = [1,0,10,'0','1',"",NaN, false, true, null, undefined,'false', 'true']
 for(var j=0; j<arr.length; j++) {
